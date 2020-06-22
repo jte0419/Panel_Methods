94eeef1e5f06ec9a492a4a6fbe27a95c5ac52fa7
Josh <joshtheengineer11@gmail.com>
2020-06-21 20:32:43 -0400

    Updated E value error handling

--- STREAMLINE_SPM.py
+++ STREAMLINE_SPM.py
@@ -7,15 +7,16 @@
 #                      - Works as expected
 #           : 02/03/19 - Transferred to Python
 #                      - Works as expected
+#           : 04/28/20 - Fixed E value error handling
 #
 # PURPOSE
 # - Compute the geometric integral at point P due to source panels
 # - Source panel strengths are constant, but can change from panel to panel
-# - Geometric integral for X-direction: Mx(ij)
-# - Geometric integral for Y-direction: My(ij)
+# - Geometric integral for X-direction: Mx(pj)
+# - Geometric integral for Y-direction: My(pj)
 #
 # REFERENCE
-# - [1]: Streamline Geometric Integral SPM, Mx(ij) and My(ij)
+# - [1]: Streamline Geometric Integral SPM, Mx(pj) and My(pj)
 #           Link: https://www.youtube.com/watch?v=BnPZjGCatcg
 #
 # INPUTS
@@ -32,6 +33,8 @@
 
 import numpy as np
 import math as math
+
+np.seterr('raise')
 
 def STREAMLINE_SPM(XP,YP,XB,YB,phi,S):
     
@@ -52,15 +55,24 @@ def STREAMLINE_SPM(XP,YP,XB,YB,phi,S):
         Cy = -np.sin(phi[j]);                                                   # C term (Y-direction)
         Dy = YP - YB[j];                                                        # D term (Y-direction)
         E  = math.sqrt(B-A**2);                                                 # E term
-        
-        # Compute Mx, Ref [1]
-        term1 = 0.5*Cx*np.log((S[j]**2 + 2*A*S[j]+B)/B);                        # First term in Mx equation
-        term2 = ((Dx-A*Cx)/E)*(math.atan2((S[j]+A),E) - math.atan2(A,E));       # Second term in Mx equation
-        Mx[j] = term1 + term2;                                                  # X-direction geometric integral
-        
-        # Compute My, Ref [1]
-        term1 = 0.5*Cy*np.log((S[j]**2 + 2*A*S[j]+B)/B);                        # First term in My equation
-        term2 = ((Dy-A*Cy)/E)*(math.atan2((S[j]+A),E) - math.atan2(A,E));       # Second term in My equation
-        My[j] = term1 + term2;                                                  # Y-direction geometric integral
+        if (E == 0 or np.iscomplex(E) or np.isnan(E) or np.isinf(E)):           # If E term is 0 or complex or a NAN or an INF
+            Mx[j] = 0                                                           # Set Mx value equal to zero
+            My[j] = 0                                                           # Set My value equal to zero
+        else:
+            # Compute Mx, Ref [1]
+            term1 = 0.5*Cx*np.log((S[j]**2 + 2*A*S[j]+B)/B);                    # First term in Mx equation
+            term2 = ((Dx-A*Cx)/E)*(math.atan2((S[j]+A),E) - math.atan2(A,E));   # Second term in Mx equation
+            Mx[j] = term1 + term2;                                              # Compute Mx integral
+            
+            # Compute My, Ref [1]
+            term1 = 0.5*Cy*np.log((S[j]**2 + 2*A*S[j]+B)/B);                    # First term in My equation
+            term2 = ((Dy-A*Cy)/E)*(math.atan2((S[j]+A),E) - math.atan2(A,E));   # Second term in My equation
+            My[j] = term1 + term2;                                              # Compute My integral
+
+        # Zero out any problem values
+        if (np.iscomplex(Mx[j]) or np.isnan(Mx[j]) or np.isinf(Mx[j])):         # If Mx term is complex or a NAN or an INF
+            Mx[j] = 0                                                           # Set Mx value equal to zero
+        if (np.iscomplex(My[j]) or np.isnan(My[j]) or np.isinf(My[j])):         # If My term is complex or a NAN or an INF
+            My[j] = 0                                                           # Set My value equal to zero
     
     return Mx, My                                                               # Return both Mx and My matrices
